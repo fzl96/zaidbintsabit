8c027703d3fa9257564627d3d7e0c27cf3477ce9
fzl96 <fpsdragon333@gmail.com>
2024-07-21 04:56:01 +0700

    feat: add utility functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,7 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { format } from "date-fns";
+import { id } from "date-fns/locale";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -9,3 +11,18 @@ export const timestamps: { createdAt: true; updatedAt: true } = {
   createdAt: true,
   updatedAt: true,
 };
+
+export function formatCurrency(amount: number) {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    minimumFractionDigits: 0,
+  }).format(amount);
+}
+
+export function formatDate(date: Date) {
+  const newDate = format(date, "eeee, d MMM yyyy", {
+    locale: id,
+  });
+  return newDate;
+}
