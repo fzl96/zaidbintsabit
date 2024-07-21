f9798472003c14c772c4541c876c9f4490fcb4a3
fzl96 <fpsdragon333@gmail.com>
2024-07-21 14:14:16 +0700

    fix: oopsies

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -2,7 +2,6 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
-const sql = neon(
-  "postgresql://fzl96:52NPyuqovMAR@ep-wild-night-a1pg0ys0.ap-southeast-1.aws.neon.tech/zaidbintsabit?sslmode=require"
-);
+const sql = neon(process.env.DATABASE_URL!);
+
 export const db = drizzle(sql, { schema });
