044c8842212e1bc0d835af546b2c5ad54a80a30c
fzl96 <fpsdragon333@gmail.com>
2024-07-23 07:51:20 +0700

    feat: add footer to dashboard layout

---
+++ src/app/(dashboard)/_components/site-footer.tsx
@@ -0,0 +1,42 @@
+import { cn } from "@/lib/utils";
+import { Icons } from "@/components/icons";
+import { siteConfig } from "@/config/site";
+import {
+  FaFacebookSquare,
+  FaYoutube,
+  FaInstagram,
+  FaFacebook,
+} from "react-icons/fa";
+
+export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
+  return (
+    <footer className={cn(className)}>
+      <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
+        <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
+          <div className="text-center md:text-left flex flex-col md:flex-row items-center text-sm gap-1 leading-loose">
+            <p className="flex items-center gap-1">
+              Copyrights
+              <Icons.copyright className="w-4 h-5" />
+              2023.
+            </p>
+            <p>All Rights Reserved by Zaid Bin Tsabit</p>
+          </div>
+        </div>
+        <div className="flex ">
+          {/* <p>Ikuti kami di </p> */}
+          <div className="flex items-center gap-2">
+            <a href={siteConfig.links.facebook} target="_blank" className=" ">
+              <FaFacebookSquare className="w-6 h-6" />
+            </a>
+            <a href={siteConfig.links.youtube} target="_blank">
+              <FaYoutube className="w-8 h-8" />
+            </a>
+            <a href={siteConfig.links.instagram} target="_blank">
+              <FaInstagram className="w-6 h-6" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { siteConfig } from "@/config/site";
 import { Nav } from "./_components/nav";
+import { SiteFooter } from "./_components/site-footer";
 
 export const metadata: Metadata = {
   title: {
@@ -19,32 +20,35 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr] md:px-10">
-      <div className="hidden border-r md:block">
-        <div className="flex h-full max-h-screen flex-col gap-2">
-          <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
-            <Link href="/" className="flex items-center gap-2 font-semibold">
-              <Image
-                src="/logo.png"
-                alt="Logo"
-                width={20}
-                height={20}
-                className="w-8 h-8"
-              />
-              <span className="">Masjid {siteConfig.name}</span>
-            </Link>
-          </div>
-          <div className="flex-1 overflow-y-auto">
-            <Nav className="grid items-start px-2 text-sm font-medium lg:px-4" />
+    <>
+      <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr] md:px-10">
+        <div className="hidden border-r md:block">
+          <div className="flex h-full max-h-screen flex-col gap-2">
+            <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+              <Link href="/" className="flex items-center gap-2 font-semibold">
+                <Image
+                  src="/logo.png"
+                  alt="Logo"
+                  width={20}
+                  height={20}
+                  className="w-8 h-8"
+                />
+                <span className="">Masjid {siteConfig.name}</span>
+              </Link>
+            </div>
+            <div className="flex-1 overflow-y-auto">
+              <Nav className="grid items-start px-2 text-sm font-medium lg:px-4" />
+            </div>
           </div>
         </div>
+        <div className="flex flex-col ">
+          <Header />
+          <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:py-6 lg:px-10">
+            {children}
+          </main>
+        </div>
       </div>
-      <div className="flex flex-col ">
-        <Header />
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:py-6 lg:px-10">
-          {children}
-        </main>
-      </div>
-    </div>
+      <SiteFooter className="border-t mt-5 md:mt-0" />
+    </>
   );
 }
